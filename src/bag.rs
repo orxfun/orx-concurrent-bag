6ce8ff34a96d9e9cb3b4e09ed7dbd390ad6c12a3
orxfun <orx.ugur.arikan@gmail.com>
2026-05-30 00:24:38 +0200

    define ptr_mut method

--- src/bag.rs
+++ src/bag.rs
@@ -890,6 +890,16 @@ where
     pub fn maximum_capacity(&self) -> usize {
         self.core.maximum_capacity()
     }
+
+    /// Returns a mutable reference to the element at the index-th position.
+    ///
+    /// # Safety
+    ///
+    /// Reading the pointer before writing its value will result in UB.
+    #[inline(always)]
+    pub unsafe fn ptr_mut(&self, idx: usize) -> *mut T {
+        unsafe { self.core.ptr_mut(idx) }
+    }
 }
 
 // HELPERS
